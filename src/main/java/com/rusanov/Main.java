844619a369b933a3623a4564fef72c4d8fabc13a
Nik <Nik@DESKTOP-8SURP8U>
2020-11-11 08:03:55 +0300

    gitignore

--- src/main/java/com/rusanov/Main.java
+++ src/main/java/com/rusanov/Main.java
@@ -4,12 +4,6 @@ import com.rusanov.commands.Command;
 import com.rusanov.commands.CreateDirCommand;
 import com.rusanov.commands.ListDirCommand;
 
-import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Main {
     public static void main(String[] args) {
         String currentDir = System.getProperty("user.dir");
